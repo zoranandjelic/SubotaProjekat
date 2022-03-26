a3fe7f6de2e67919ca933bc4f5e6a476335b3565
ratkorajkovic <45713322+ratkorajkovic@users.noreply.github.com>
2022-03-26 11:19:55 +0100

    Add files via upload

---
+++ src/main/java/com/example/vjezbanjezagit/VjezbanjeZaGitApplication.java
@@ -0,0 +1,14 @@
+package com.example.vjezbanjezagit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VjezbanjeZaGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VjezbanjeZaGitApplication.class, args);
+        System.out.println("Danas je lijep dan!");
+    }
+
+}
---
+++ src/test/java/com/example/vjezbanjezagit/VjezbanjeZaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vjezbanjezagit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VjezbanjeZaGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
